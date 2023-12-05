078c7d42aefa3dbdb9ee4b92bcfa6b31331458d0
Joaquin Arlind <joaquinarlind26@gmail.com>
2023-12-05 18:49:52 +0700

    Google Authentication

--- app/src/main/java/com/example/cookmark_app/Landing.java
+++ app/src/main/java/com/example/cookmark_app/Landing.java
@@ -1,14 +1,34 @@
 package com.example.cookmark_app;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.IntentSenderRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.BeginSignInResult;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class Landing extends AppCompatActivity {
-
+    private SignInClient oneTapClient;
+    private BeginSignInRequest signUpRequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,12 +53,51 @@ public class Landing extends AppCompatActivity {
         });
 
         Button signInGoogleButton = findViewById(R.id.buttonSignInGoogle);
+        ActivityResultLauncher<IntentSenderRequest> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartIntentSenderForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if(result.getResultCode() == Activity.RESULT_OK){
+                    try{
+                        SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(result.getData());
+                        String idToken = credential.getGoogleIdToken();
+                        if(idToken != null){
+                            String email = credential.getId();
+                            Toast.makeText(getApplicationContext(), "Email: " + email, Toast.LENGTH_SHORT).show();
+                        }
+                    }catch(ApiException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
         signInGoogleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // masukin logic sign in with google
+                oneTapClient.beginSignIn(signUpRequest)
+                        .addOnSuccessListener(Landing.this, new OnSuccessListener<BeginSignInResult>(){
+                            @Override
+                            public void onSuccess(BeginSignInResult result) {
+                                IntentSenderRequest intentSenderRequest = new IntentSenderRequest.Builder(result.getPendingIntent().getIntentSender()).build();
+                                activityResultLauncher.launch(intentSenderRequest);
+                            }
+                        })
+                        .addOnFailureListener(Landing.this, new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d("TAG", e.getLocalizedMessage());
+                            }
+                        });
             }
         });
+
+        oneTapClient = Identity.getSignInClient(this);
+        signUpRequest = BeginSignInRequest.builder()
+                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        .setServerClientId(getString(R.string.web_client_id))
+                        .setFilterByAuthorizedAccounts(false)
+                        .build())
+                .build();
 
     }
 }
