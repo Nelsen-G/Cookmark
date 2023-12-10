016524fb9c95491bc71919a391280efce97892a9
Nelsen-G <nelsengabriel@gmail.com>
2023-12-10 16:57:16 +0700

    feature upload

---
+++ app/src/main/java/com/example/cookmark_app/model/Ingredient.java
@@ -0,0 +1,13 @@
+package com.example.cookmark_app.model;
+
+public class Ingredient {
+    private String name;
+
+    public Ingredient(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
