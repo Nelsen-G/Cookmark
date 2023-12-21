0e1a67a7e76038c78cc78c1ebb1966dabb1f566b
Natasha Radika <81007696+tashagrc@users.noreply.github.com>
2023-12-21 19:26:30 +0700

    search ui

--- app/src/main/java/com/example/cookmark_app/fragment/SearchFragment.java
+++ app/src/main/java/com/example/cookmark_app/fragment/SearchFragment.java
@@ -9,6 +9,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import com.example.cookmark_app.R;
 import com.example.cookmark_app.adapter.IngredientAdapter;
@@ -19,7 +21,11 @@ import java.util.ArrayList;
 
 public class SearchFragment extends Fragment {
 
-   private ArrayList<Ingredient> ingredients;
+    private ArrayList<Ingredient> ingredients;
+    private ArrayList<String> ingredientAll;
+    private ArrayAdapter<String> adapter;
+    AutoCompleteTextView searchBar;
+    View rootView;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -34,17 +40,21 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_search, container, false);
-        initializeRecyclerView(rootView, R.id.rvIngredientsData);
+        rootView = inflater.inflate(R.layout.fragment_search, container, false);
+        initializeRecyclerView(rootView, R.id.rvIngredientsData1, getIngredientSet1());
+        initializeRecyclerView(rootView, R.id.rvIngredientsData2, getIngredientSet1());
+        initializeRecyclerView(rootView, R.id.rvIngredientsData3, getIngredientSet1());
+        initializeRecyclerView(rootView, R.id.rvIngredientsData4, getIngredientSet1());
+        initializeRecyclerView(rootView, R.id.rvIngredientsData5, getIngredientSet1());
+
+        searchBar = rootView.findViewById(R.id.ingredientSearchBar);
+        ingredientAll = new ArrayList<>();
+        initializeAutoCompleteList();
         return rootView;
 
     }
 
-    private void initializeRecyclerView(View rootView, int recyclerViewId) {
-        ingredients = new ArrayList<>();
-        ingredients.add(new Ingredient("Cheese"));
-        ingredients.add(new Ingredient("Salt"));
-        ingredients.add(new Ingredient("Egg"));
+    private void initializeRecyclerView(View rootView, int recyclerViewId, ArrayList<Ingredient> ingredients) {
 
         RecyclerView recyclerView = rootView.findViewById(recyclerViewId);
 
@@ -55,5 +65,27 @@ public class SearchFragment extends Fragment {
 
         recyclerView.setAdapter(tagTypeAdapter);
 
+    }
+
+    private ArrayList<Ingredient> getIngredientSet1() {
+        ArrayList<Ingredient> ingredients = new ArrayList<>();
+        ingredients.add(new Ingredient("Cheese"));
+        ingredients.add(new Ingredient("Salt"));
+        ingredients.add(new Ingredient("Egg"));
+
+        return ingredients;
+    }
+
+    private ArrayList<Ingredient> getIngredientsSet2() {
+        // Define ingredients for the second set
+        return null;
+    }
+
+    private void initializeAutoCompleteList() {
+        ingredientAll.add("Cheese");
+        ingredientAll.add("Salt");
+        ingredientAll.add("Egg");
+        adapter = new ArrayAdapter<>(rootView.getContext(), android.R.layout.simple_list_item_1, ingredientAll);
+        searchBar.setAdapter(adapter);
     }
 }
