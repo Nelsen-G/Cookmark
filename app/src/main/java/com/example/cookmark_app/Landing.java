8e73e5e6123d69e737262d6f4bcbfaabdafaa33f
Joaquin Arlind <joaquinarlind26@gmail.com>
2023-12-27 00:15:35 +0700

    Settings done, about otw. Fixed some code warnings

--- app/src/main/java/com/example/cookmark_app/Landing.java
+++ app/src/main/java/com/example/cookmark_app/Landing.java
@@ -1,48 +1,33 @@
 package com.example.cookmark_app;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.IntentSenderRequest;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.identity.BeginSignInRequest;
-import com.google.android.gms.auth.api.identity.BeginSignInResult;
-import com.google.android.gms.auth.api.identity.Identity;
-import com.google.android.gms.auth.api.identity.SignInClient;
-import com.google.android.gms.auth.api.identity.SignInCredential;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptionsExtension;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.messaging.FirebaseMessaging;
 
 import java.util.HashMap;
 import java.util.Objects;
@@ -96,9 +81,22 @@ public class Landing extends AppCompatActivity {
     }
 
     private void googleSignIn() {
+        signOutGoogleAccount();
         Intent intent = googleSignInClient.getSignInIntent();
-        startActivityForResult(intent, RC_SIGN_IN);
+        signInLauncher.launch(intent);
     }
+
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    if (data != null) {
+                        int requestCode = RC_SIGN_IN;
+                        onActivityResult(requestCode, Activity.RESULT_OK, data);
+                    }
+                }
+            });
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -142,6 +140,15 @@ public class Landing extends AppCompatActivity {
                         }
                     }
                 });
+    }
+
+    private void signOutGoogleAccount() {
+        googleSignInClient.signOut().addOnCompleteListener(this, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                // Handle sign-out completion if needed
+            }
+        });
     }
 
     protected void onStart() {
--- app/src/main/java/com/example/cookmark_app/fragment/AccountFragment.java
+++ app/src/main/java/com/example/cookmark_app/fragment/AccountFragment.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,19 +14,18 @@ import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
 import com.example.cookmark_app.About;
 import com.example.cookmark_app.Landing;
-import com.example.cookmark_app.MainActivity;
 import com.example.cookmark_app.ManageRecipe;
 import com.example.cookmark_app.R;
 import com.example.cookmark_app.dialog.EditNameDialog;
 import com.example.cookmark_app.dialog.EditPasswordDialog;
 import com.google.android.material.button.MaterialButton;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -40,10 +38,12 @@ import java.util.UUID;
 public class AccountFragment extends Fragment implements EditNameDialog.OnUsernameUpdatedListener {
     private String userId;
     private ActivityResultLauncher<Intent> pickImageLauncher;
-    private FirebaseStorage storage = FirebaseStorage.getInstance();
+    private final FirebaseStorage storage = FirebaseStorage.getInstance();
     private StorageReference storageRef = storage.getReference();
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    FirebaseUser currentUser = firebaseAuth.getCurrentUser();
     View view;
-    TextView userNameTxt, userEmailTxt;
+    TextView userNameTxt, userEmailTxt, profileSettingsTxt;
     ImageView profilePictureImageView;
 
     public AccountFragment() {
@@ -60,11 +60,63 @@ public class AccountFragment extends Fragment implements EditNameDialog.OnUserna
             userId = bundle.getString("user_id");
         }
 
+        profileSettingsTxt = view.findViewById(R.id.profileSettings);
         userNameTxt = view.findViewById(R.id.userName);
         userEmailTxt = view.findViewById(R.id.userEmail);
         profilePictureImageView = view.findViewById(R.id.profilePicture);
 
-        retrieveUserInfoFromFirestore(userId, profilePictureImageView);
+        Button editPic = view.findViewById(R.id.editProfilePicture);
+        Button editUsername = view.findViewById(R.id.editUsername);
+        Button editPassword = view.findViewById(R.id.editPassword);
+
+        editPic.setVisibility(View.GONE);
+        editUsername.setVisibility(View.GONE);
+        editPassword.setVisibility(View.GONE);
+        profileSettingsTxt.setVisibility(View.GONE);
+
+        if (currentUser != null) {
+            userNameTxt.setText(currentUser.getDisplayName());
+            userEmailTxt.setText(currentUser.getEmail());
+            if (currentUser.getPhotoUrl() != null) {
+                Glide.with(requireActivity())
+                        .load(currentUser.getPhotoUrl())
+                        .into(profilePictureImageView);
+            }
+        } else {
+            retrieveUserInfoFromFirestore(userId, profilePictureImageView);
+            editPic.setVisibility(View.VISIBLE);
+            editUsername.setVisibility(View.VISIBLE);
+            editPassword.setVisibility(View.VISIBLE);
+            profileSettingsTxt.setVisibility(View.VISIBLE);
+
+            editPic.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (currentUser == null){
+                        openGallery();
+                    }
+                }
+            });
+
+            editUsername.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (currentUser == null){
+                        openEditNameDialog(userId);
+                    }
+                }
+            });
+
+            editPassword.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (currentUser == null){
+                        openEditPasswordDialog(userId);
+                    }
+                }
+            });
+
+        }
 
         pickImageLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
@@ -102,8 +154,8 @@ public class AccountFragment extends Fragment implements EditNameDialog.OnUserna
         aboutPage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), About.class);
-                startActivity(intent);
+                Intent intentAbout = new Intent(getActivity(), About.class);
+                startActivity(intentAbout);
             }
         });
 
@@ -115,33 +167,6 @@ public class AccountFragment extends Fragment implements EditNameDialog.OnUserna
                 showToast("You have been logged out");
                 Intent it = new Intent(getActivity(), Landing.class);
                 startActivity(it);
-            }
-        });
-
-        Button editPic = view.findViewById(R.id.editProfilePicture);
-
-        editPic.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openGallery();
-            }
-        });
-
-        Button editUsername = view.findViewById(R.id.editUsername);
-
-        editUsername.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openEditNameDialog(userId);
-            }
-        });
-
-        Button editPassword = view.findViewById(R.id.editPassword);
-
-        editPassword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openEditPasswordDialog(userId);
             }
         });
 
@@ -265,35 +290,4 @@ public class AccountFragment extends Fragment implements EditNameDialog.OnUserna
     private void showToast(String message) {
         Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
     }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        hideToolbar();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        showToolbar();
-    }
-
-    private void hideToolbar() {
-        if (view != null) {
-            Toolbar toolbar = view.findViewById(R.id.toolbar);
-            if (toolbar != null) {
-                toolbar.setVisibility(View.GONE);
-            }
-        }
-    }
-
-    private void showToolbar() {
-        if (view != null) {
-            Toolbar toolbar = view.findViewById(R.id.toolbar);
-            if (toolbar != null) {
-                toolbar.setVisibility(View.VISIBLE);
-            }
-        }
-    }
-
 }
