cc0ce7e8a845e550140cfe08bccfe2a4427d9300
Natasha Radika <81007696+tashagrc@users.noreply.github.com>
2023-12-31 23:18:39 +0700

    add widget with dummy data

---
+++ app/src/main/java/com/example/cookmark_app/widget/TodayMenuWidget.java
@@ -0,0 +1,58 @@
+package com.example.cookmark_app.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import com.example.cookmark_app.R;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class TodayMenuWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.today_menu_widget);
+        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+        for (int i = 0; i < appWidgetIds.length; ++i) {
+            Intent intent = new Intent(context, ListWidgetService.class);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+
+            intent.setData(Uri.parse(intent.toUri(intent.URI_INTENT_SCHEME)));
+
+            // Construct the RemoteViews object
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.today_menu_widget);
+            views.setRemoteAdapter(R.id.list_view, intent);
+
+            views.setEmptyView(R.id.list_view,R.id.empty_view);
+
+            appWidgetManager.updateAppWidget(appWidgetIds, views);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
